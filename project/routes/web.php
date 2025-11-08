255d4f318e2319cb86305931b87ded0a1633fed5
Akmalraze <165366805+Akmalraze@users.noreply.github.com>
2025-11-08 14:39:42 +0800

    hai
    asa

--- project/routes/web.php
+++ project/routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/Home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/About', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/Contact', function () {
+    return view('contact');
+})->name('contact');
+
+
